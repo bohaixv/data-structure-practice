63ac2e257478ecc3684dc4db3155e76bbc6caa57
jack bai <1146873420@qq.com>
2019-08-30 09:31:09 +0800

    feat: binary search tree

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -12,5 +12,30 @@ class BinarySearchTree {
     this.root = new Node(key)
   }
 
+  insertNode (key, node = this.root) {
+    if (key > node.key) {
+      if (node.right === null) {
+        node.right = new Node(key)
+      } else {
+        this.insertNode(key, node.right)
+      }
+    } else {
+      if (node.left === null) {
+        node.left = new Node(key)
+      } else {
+        this.insertNode(key, node.left)
+      }
+    }
+  }
 
 }
+
+
+const binarySearchTree = new BinarySearchTree(8)
+
+binarySearchTree.insertNode(5)
+binarySearchTree.insertNode(6)
+binarySearchTree.insertNode(9)
+binarySearchTree.insertNode(10)
+
+console.log('binary search tree:', binarySearchTree)
